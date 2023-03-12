575e2bc84e800fb6ffd136d02304ba7525bec081
TYRONE AVNIT <tyronemichael@gmail.com>
2023-03-12 06:41:39 +0200

    Fix adding addresses to DID

--- app/src/pages/squads/create.tsx
+++ app/src/pages/squads/create.tsx
@@ -1,7 +1,8 @@
 import { BitwiseVerificationMethodFlag, DidSolIdentifier, DidSolService, VerificationMethodType } from '@identity.com/sol-did-client'
 import { mdiBallotOutline, mdiWallet } from '@mdi/js'
+import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { PublicKey } from '@solana/web3.js'
+import { Keypair, PublicKey } from '@solana/web3.js'
 import { Field, Form, Formik } from 'formik'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
@@ -24,6 +25,7 @@ import LayoutApp from '../../layouts/App'
 import { createSquad } from '../../services/squads'
 import { useAppDispatch } from '../../stores/hooks'
 import { pushMessage } from '../../stores/snackBarSlice'
+import { getSquadsForWallet } from '../../utils/wallet'
 
 const CreateSquad = () => {
   const dispatch = useAppDispatch()
@@ -45,26 +47,28 @@ const CreateSquad = () => {
 
   const addSquadToDid = async (squad?: Squad) => {
     const didSolIdentifier = DidSolIdentifier.create(wallet.publicKey, 'devnet');
-    const service = DidSolService.build(
-      didSolIdentifier,
-      {
-        connection,
-      }
-    );
+    const service = await DidSolService.build(didSolIdentifier);
 
     await service
       .addVerificationMethod({
-        fragment: "squad",
-        keyData: Buffer.from(new PublicKey(squad?.address).toBytes()),
+        fragment: "4",
+        keyData: Buffer.from((new PublicKey(squad.address)).toBytes()),
         methodType: VerificationMethodType.Ed25519VerificationKey2018,
-        flags: [BitwiseVerificationMethodFlag.CapabilityDelegation],
+        flags: [BitwiseVerificationMethodFlag.CapabilityInvocation],
       }, wallet.publicKey)
-      .withAutomaticAlloc(wallet.publicKey)
       .withSolWallet(wallet)
+      .withAutomaticAlloc(wallet.publicKey)
       .rpc();
   }
 
   const createSquadAction = async ({ partner, requireBothApproval }) => {
+    const t = await getSquadsForWallet(wallet, connection)
+    try {
+      console.log(t);
+    } catch(error) {
+      console.log(error);
+    }
+    
     const threshold = requireBothApproval ? 2 : 1
     setIsCreating(true)
     createSquad(partner, threshold)
@@ -74,6 +78,7 @@ const CreateSquad = () => {
         dispatch(pushMessage(message))
       })
       .catch((error) => {
+        console.log(error);
         const message = createSnackbarMessage(`Failed to create squad`, false)
         dispatch(pushMessage(message))
       })
